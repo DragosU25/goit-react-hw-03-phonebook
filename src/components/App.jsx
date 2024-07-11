496afb3d2dad8186e3cf91c082c4c9a4e6c5b7f4
DragosU25 <149568004+DragosU25@users.noreply.github.com>
2024-07-11 21:07:14 +0200

    add the contacts in local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,26 @@ import styles from '../App.module.css';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleFilterChange = event => {
     this.setState({ filter: event.target.value });
